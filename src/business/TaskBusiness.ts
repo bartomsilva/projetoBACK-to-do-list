560ed5e10597592965b5d65499b960d4d3042beb
bartomsilva <bartomsilva@gmail.com>
2023-10-31 23:34:49 -0300

    removi oi ; tipagem do getTasks

--- src/business/TaskBusiness.ts
+++ src/business/TaskBusiness.ts
@@ -1,11 +1,11 @@
 // taskBusiness.ts
-import { Task } from "../model/Task";
-import { TASK_STATUS } from "../model/Status";
-import { IdGenerator } from "../services/IdGenerator";
-import { NotFoundError } from "../error/NotFound";
-import { BadRequestError } from "../error/BadRequest";
-import { GetTasksInputDTO } from "../dtos/getTasks.dto";
-import { UpdateTaskInputDTO } from "../dtos/updateTask.dto";
+import { Task } from "../model/Task"
+import { TASK_STATUS } from "../model/Status"
+import { IdGenerator } from "../services/IdGenerator"
+import { NotFoundError } from "../error/NotFound"
+import { BadRequestError } from "../error/BadRequest"
+import { GetTasksInputDTO } from "../dtos/getTasks.dto"
+import { UpdateTaskInputDTO } from "../dtos/updateTask.dto"
 
 export class TaskBusiness {
   constructor(private idGenerator: IdGenerator) { }
@@ -19,39 +19,42 @@ export class TaskBusiness {
     return await Task.create(newTask)
   }
 
-  public getTasks = async (input: GetTasksInputDTO): Promise<any> => {
+  public getTasks = async (input: GetTasksInputDTO): Promise<Task[]> => {
     switch (true) {
       case (input.id !== undefined):
-        return await Task.findByPk(input.id)
+        return await Task.findAll({
+          where: {id: input.id}})
       case (input.id === undefined && input.status===undefined):
-        return await Task.findAll();
+        return await Task.findAll()
       case (input.status !== undefined):
         return await Task.findAll({
-          where: { status: input.status }
+          where: {status: input.status }
         })
+      default:
+        return []
     }
   }
 
   public updateTask = async (id: string, updateTask: UpdateTaskInputDTO) => {
-    const task = await Task.findByPk(id);
+    const task = await Task.findByPk(id)
     const { description, date, time } = updateTask
     if (!task) {
-      throw new NotFoundError('Tarefa não encontrada');
+      throw new NotFoundError('Tarefa não encontrada')
     }
     if(!task.status.includes(TASK_STATUS.OPEN) && !task.status.includes(TASK_STATUS.PAUSED)){
       throw new BadRequestError("O Status da Task não permite alteração")
     }
-    task.description = description ?? task.description;
+    task.description = description ?? task.description
     task.date = date ?? task.date
     task.time = time ?? task.time
-    return await task.save();
+    return await task.save()
   }
 
   public deleteTask = async (id: string) => {
-    const task = await Task.findByPk(id);
+    const task = await Task.findByPk(id)
     if (!task) {
-      throw new BadRequestError('Tarefa não encontrada');
+      throw new BadRequestError('Tarefa não encontrada')
     }
-    await task.destroy();
+    await task.destroy()
   }
 }
